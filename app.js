122a3ca1e567cb4230cc2a0d078bb50130f69549
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-05-08 22:45:28 +0200

    Add bugs and some ideas

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+let fullOperation = [];
+let indexCurrentOperation = 2;
+
+let fullOperationDisplay = document.querySelector('.full-operation');
+let currentDisplayNumber = document.querySelector('.current-number');
+const btns = document.querySelectorAll('.buttons');
+
+function updateFullOperationDisplay() {
+  fullOperationDisplay.textContent = '';
+  fullOperation.forEach(
+    (num) => (fullOperationDisplay.textContent += ' ' + num)
+  );
+}
+
+function updateCurrentDisplayNumber(num) {
+  currentDisplayNumber.textContent += num;
+}
+
+function getResult(operator, num) {
+  let result = num;
+
+  if (indexCurrentOperation + 1 > fullOperation.length) {
+    return result;
+  } else {
+    result = operate(operator, num, fullOperation[indexCurrentOperation]);
+    indexCurrentOperation += 2;
+  }
+
+  return result;
+}
+
 function operate(operator, num1, num2) {
   switch (operator) {
     case '%':
@@ -10,29 +41,36 @@ function operate(operator, num1, num2) {
       return num1 - num2;
     case '+':
       return num1 + num2;
+  }
+}
+
+function clear() {
+  fullOperation = [];
+  indexCurrentOperation = 2;
+  currentDisplayNumber.textContent = '';
+}
+
+function startBtnAction(btnContent) {
+  switch (btnContent) {
+    case '%':
+    case '+':
+    case '-':
+    case '÷':
+    case '×':
+      fullOperation.push(currentDisplayNumber.textContent);
+      fullOperation.push(btnContent);
+      updateFullOperationDisplay();
+      updateCurrentDisplayNumber(getResult(btnContent, currentDisplayNumber.textContent));
+      break;
+    case 'AC':
+      clear();
+      break;
     default:
-      console.log("Unknown Operator");
+      updateCurrentDisplayNumber(btnContent);
       break;
   }
 }
 
-function updateDisplay(pressedBtn) {
-  if (empty) {
-    display.textContent = pressedBtn;
-    empty = false;
-  } else {
-    display.textContent += pressedBtn;
-  }
-
-  
-}
-
-let empty = true;
-
-const display = document.querySelector('.display');
-const operators = document.querySelectorAll('.operator');
-const numbers = document.querySelectorAll('.number');
-
-numbers.forEach(num => num.addEventListener('click', e => updateDisplay(e.target.textContent)));
-
-operators.forEach(operator => operator.addEventListener('click', e => operate(e.target.textContent, 4, 5)));
+btns.forEach((btn) =>
+  btn.addEventListener('click', (e) => startBtnAction(e.target.textContent))
+);
