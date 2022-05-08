1eb3dfd81acc44e5b911bdcc53312e4049c3ee9d
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-05-08 18:04:05 +0200

    Add function for displaying numbers

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ function operate(operator, num1, num2) {
   }
 }
 
+function updateDisplay(pressedBtn) {
+  if (empty) {
+    display.textContent = pressedBtn;
+    empty = false;
+  } else {
+    display.textContent += pressedBtn;
+  }
+
+  
+}
+
+let empty = true;
+
+const display = document.querySelector('.display');
 const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(num => num.addEventListener('click', e => updateDisplay(e.target.textContent)));
 
 operators.forEach(operator => operator.addEventListener('click', e => operate(e.target.textContent, 4, 5)));
